17b70b1b25922f85af99d8ec2f10ad961d0e27db
ABDUL RASHEED N P <rashiidrashi5@gmail.com>
2025-10-11 11:03:49 +0530

    update

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import PostAuthor from '../components/PostAuthor'
+import { Link } from 'react-router-dom'
 
 const PostDetail = () => {
   return (
-    <div>
-      
-    </div>
+     <section className="post-detail">
+       <div className="container post-detail__container">
+        <div className="post-deatail__header">
+            <PostAuthor/>
+            <div className="post-detail__buttons">
+              <Link to={`/posts/wewer/edit`} className='btn sm primary'>Edit</Link>
+              <Link to={`/posts/wewer/delete`} className='btn sm danger'>Delete</Link>
+
+            </div>
+        </div>
+       </div>
+     </section>
   )
 }
 
