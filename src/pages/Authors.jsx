3cb1bc677b8304e8ac5f258ec54acb89f3c9b8d6
ABDUL RASHEED N P <rashiidrashi5@gmail.com>
2025-10-16 11:57:15 +0530

    update

--- src/pages/Authors.jsx
+++ src/pages/Authors.jsx
@@ -1,4 +1,17 @@
 import React from 'react'
+import Avatar1 from '../images/avatar1.jpg'
+import Avatar2 from '../images/avatar2.jpg'
+import Avatar3 from '../images/avatar3.jpg'
+import Avatar4 from '../images/avatar4.jpg'
+import Avatar5 from '../images/avatar.jpg'
+
+const authorsData = [
+  {id: 1, avatar: Avatar1, name: 'Ayra raay' , posts: 3},
+  {id: 2, avatar: Avatar2, name: 'Rutvi raay' , posts: 5},
+  {id: 3, avatar: Avatar3, name: 'Rayuu raay ' , posts: 0},
+  {id: 4, avatar: Avatar4, name: 'Ayana M P ' , posts: 2},
+  {id: 1, avatar: Avatar5, name: 'Rashi N P  ' , posts: 1},
+]
 
 const Authors = () => {
   return (
